9f517817dcd173cd476ca8909349d7d5e2d51c40
seobei <sys1774@nate.com>
2021-08-11 17:21:46 +0900

    User

--- src/main/java/org/zerock/service/UserService.java
+++ src/main/java/org/zerock/service/UserService.java
@@ -1,9 +1,5 @@
 package org.zerock.service;
 
-import java.util.List;
-
-import org.zerock.domain.Criteria;
-import org.zerock.domain.MessageVO;
 import org.zerock.domain.UserVO;
 
 public interface UserService {
@@ -19,26 +15,22 @@ public interface UserService {
 	//데이터 수정 
 	boolean modify(UserVO vo);
 	boolean modifyPassword(UserVO vo, String oldPassword);
-	boolean modifyPassword(UserVO vo);
+	boolean modifyPw(UserVO vo);
 	
 	//데이터 삭제 
 	boolean remove(UserVO vo);
 	boolean remove(UserVO vo, String inputPassword);
-	
-	
 	
 	//아이디 찾기
 	UserVO findId(UserVO vo);
 
 	//비밀번호 찾기
 	UserVO findPw(UserVO vo);
-	
 
 	//비밀번호 변경
 	boolean updatePw (UserVO vo);
 
 	//이메일발송
 	public void sendEmail(UserVO vo, String div) throws Exception;
-	
 
 }
